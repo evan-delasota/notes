8b9dd6edaf15aaca9486cec41a8cafe9822ca5ef
Evan D <evan.delasota@gmail.com>
2021-03-02 02:16:14 -0800

    Added validator and chalk packages, removed utils

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const fs = require('fs')
+const validator = require('validator');
+const chalk = require('chalk');
+const notes = require('./notes.js');
 
-fs.writeFileSync("notes.txt", "This file was created by Node.js\n");
+const successfulPrint = chalk.bold.green;
+const failedPrint = chalk.bold.red;
 
-fs.appendFileSync('notes.txt', 'Appending this message to the file.\n');
+const msg = notes();
+const url = 'https://mead.io';
+const badUrl = 'https:/mead.io';
+
+console.log(msg);
+console.log(successfulPrint(validator.isEmail('username@example.com')));
+console.log(successfulPrint(validator.isURL(url)));
+
+
+if (validator.isURL(badUrl)) {
+    console.log(successfulPrint('true')); 
+} else {
+    console.log(failedPrint('false'));   
+}
