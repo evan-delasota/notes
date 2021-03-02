fa5051f0d5ab923212634f95c6abd39f9673a276
Evan D <evan.delasota@gmail.com>
2021-03-02 01:24:59 -0800

    Added append method

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
-fs.writeFileSync("notes.txt", "This file was created by Node.js");
+fs.writeFileSync("notes.txt", "This file was created by Node.js\n");
+
+fs.appendFileSync('notes.txt', 'Appending this message to the file.\n');
