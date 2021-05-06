b86aecf33ca8e4856f802e0fc7f7c5324fcdd6c9
Evan Delasota <evan_delasota@yahoo.com>
2021-05-05 19:39:40 -0700

    Added loadNotes and addNotes method to notes.js

--- notes.js
+++ notes.js
@@ -1,5 +1,25 @@
+const fs = require('fs')
+
 const getNotes = function() {
     return 'Your notes...\n';
 }
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+    const notes = loadNotes();
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        
+        return JSON.parse(dataJSON);
+    } catch(e) {
+        return [];
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+};
