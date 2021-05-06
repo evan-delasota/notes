b8c3576cbb89498929b2495cbac7ecff06c1a33d
Evan Delasota <evan_delasota@yahoo.com>
2021-05-05 19:52:03 -0700

    Modified addNote handler method

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const validator = require('validator');
 const chalk = require('chalk');
 const yargs = require('yargs');
 const notes = require('./notes.js');
+const { addNote } = require('./notes.js');
 
 // Customize yargs version
 yargs.version('1.1.0');
@@ -24,8 +25,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Body: ' + argv.body);
+        notes.addNote(argv.title, argv.body);
     }
 })
 
